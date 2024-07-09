10483cd0c4e035faaa1b921a10d964dc61787cde
Vietng <vietng322611@gmail.com>
2024-07-09 21:52:02 +0700

    Refactor: Add entry files

---
+++ app/src/main/java/com/chat4osu/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.chat4osu.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
