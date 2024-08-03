7a182884c13a106c909658add1232433beb42df5
Vietng <vietng322611@gmail.com>
2024-08-03 12:45:10 +0700

    Feat: Remove and add more methods

--- app/src/main/java/com/chat4osu/osu/AppSocket.kt
+++ app/src/main/java/com/chat4osu/osu/AppSocket.kt
@@ -1,14 +1,24 @@
 package com.chat4osu.osu
 
+import androidx.compose.runtime.mutableStateListOf
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class AppSocket {
     private var osuSocket = OsuSocket()
 
+    private val patterns = listOf(
+        Regex("/raw (.*)") to { match: String -> sendRaw(match) },
+        Regex("/join (.*)") to { match: String -> getDM(match) },
+        Regex("/join #(.*)") to { match: String -> osuSocket.join("#$match") },
+        Regex("/part #(.*)") to { match: String -> osuSocket.part("#$match") },
+        Regex("!mp make (.*)") to { match: String -> createMatch(match) }
+    )
+
     suspend fun connect(nick: String, pass: String): Int {
         return withContext(Dispatchers.IO) {
-            osuSocket.connect(nick, pass)
+            val code = osuSocket.connect(nick, pass)
+            code
         }
     }
 
@@ -18,21 +28,74 @@ class AppSocket {
         } catch (e: NoSuchElementException) { "" }
     }
 
-    fun join(channelName: String) { osuSocket.join(channelName) }
+    fun readInput(input: String) {
+        for ((pattern, action) in patterns) {
+            if (!pattern.matches(input)) continue
 
-    fun part(channelName: String) { osuSocket.part(channelName) }
+            val match = pattern.find(input)
+            if (match != null) {
+                action(match.groupValues[1])
+                println(collect())
+                return
+            }
+        }
 
-    fun getMessage(channelName: String?): List<String>? {
-        val channel = osuSocket.manager.getChannel(channelName)
-
-        if (channel != null) { return channel.getMessage() }
-        else { return null }
+        send(input)
     }
 
-    fun getUser(channelName: String): Set<String>? {
-        val channel = osuSocket.manager.getChannel(channelName)
+    private fun send(msg: String) {
+        osuSocket.send("PRIVMSG ${osuSocket.manager.activeChat} $msg")
+    }
 
-        if (channel != null) { return channel.user }
-        else { return null }
+    private fun getDM(user: String) {
+        osuSocket.manager.addChat(user)
+        setActiveChat(user)
+    }
+
+    private fun sendRaw(msg: String) {
+        osuSocket.send(msg)
+    }
+
+    private fun createMatch(name: String) {
+        send("!mp make $name")
+//        if (osuSocket.manager.activeChat.equals("BanchoBot")) {}
+    }
+
+    fun getMessage(channelName: String): MutableList<String> {
+        osuSocket.manager.getChannel(channelName)?.let {
+            return it.message
+        }
+
+        return mutableStateListOf()
+    }
+
+    fun saveMsg(msgList: List<String>) {
+        osuSocket.manager.getChannel("")?.saveMsg(msgList)
+    }
+
+    fun getUser(channelName: String): Set<String> {
+        osuSocket.manager.getChannel(channelName)?.let {
+            return it.user
+        }
+        return mutableSetOf()
+    }
+
+    fun getRoot(): String {
+        return osuSocket.manager.nick
+    }
+
+    fun getActiveChat(): String {
+        osuSocket.manager.activeChat?.let {
+            return osuSocket.manager.activeChat
+        }
+        return ""
+    }
+
+    fun getAllChat(): List<String> {
+        return osuSocket.manager.allChat
+    }
+
+    fun setActiveChat(name: String) {
+        osuSocket.manager.setActiveChat(name)
     }
 }
