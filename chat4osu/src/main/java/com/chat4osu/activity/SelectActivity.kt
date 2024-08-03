f72b9776fd559aa91ac57f26dbcc54bfbec8eabe
Vietng <vietng322611@gmail.com>
2024-08-03 22:26:53 +0700

    Test: Adding way to remove channels

--- chat4osu/src/main/java/com/chat4osu/activity/SelectActivity.kt
+++ chat4osu/src/main/java/com/chat4osu/activity/SelectActivity.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -24,11 +29,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import androidx.wear.compose.material.FractionalThreshold
+import androidx.wear.compose.material.rememberSwipeableState
+import androidx.wear.compose.material.swipeable
 import com.chat4osu.di.AppModule
 import com.chat4osu.ui.theme.Chat4osuTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class SelectActivity: ComponentActivity() {
@@ -62,17 +74,7 @@ class SelectActivity: ComponentActivity() {
                 ) {
                     AppModule.socket.getAllChat().let {
                         for (name: String in it) {
-                            Button(
-                                onClick = {
-                                    AppModule.socket.setActiveChat(name)
-                                    navigateToChat()
-                                },
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(5.dp)
-                            ) {
-                                Text(text = name)
-                            }
+                            SwipeableButton(name = name)
                         }
                     }
                 }
@@ -87,6 +89,50 @@ class SelectActivity: ComponentActivity() {
                     navigateToChat()
                 }
             )
+        }
+    }
+
+    @OptIn(ExperimentalWearMaterialApi::class)
+    @Composable
+    fun SwipeableButton(name: String) {
+        val squareSize = 56.dp
+
+        val swipeState = rememberSwipeableState(0)
+        val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+        val anchors = mapOf(0f to 0, sizePx*2 to 1)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .swipeable(
+                    state = swipeState,
+                    anchors = anchors,
+                    thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                    orientation = Orientation.Horizontal
+                )
+        ) {
+            Button(
+                onClick = {
+                    AppModule.socket.setActiveChat(name)
+                    navigateToChat()
+                },
+                modifier = Modifier
+                    .offset { IntOffset(swipeState.offset.value.roundToInt(), 0) }
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ) {
+                Text(text = name)
+            }
+
+            if (swipeState.currentValue == 1) {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Clear,
+                        contentDescription = "Delete chat"
+                    )
+                }
+            }
         }
     }
 
